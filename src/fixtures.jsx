56c27b9eb033ea7b8eee6874714b5d6da4a84c33
Joao Guilherme Almeida <joaogcfa@al.insper.edu.br>
2020-11-03 10:34:46 -0300

    faz o get da api

--- src/fixtures.jsx
+++ src/fixtures.jsx
@@ -8,17 +8,19 @@ export default function Fixtures() {
     { username: "bananajohson", key: 1 },
     { username: "jack", key: 2 },
   ]);
-  axios
-    .get(
-      "live.json?key=pspen0saaIX6HcUE&secret=9hSYKcugfodheluDNTSbUkl43jbKw5oF&competition_id=244",
-      {}
-    )
-    .then((resp) => {
-      if (Math.floor(resp.status / 100 === 2)) {
-        console.log(resp);
-        return;
-      }
-    });
+  const cors = require("cors");
+  useEffect(() => {
+    axios
+      .get(
+        "https://cors-anywhere.herokuapp.com/http://livescore-api.com/api-client/fixtures/matches.json?key=pspen0saaIX6HcUE&secret=9hSYKcugfodheluDNTSbUkl43jbKw5oF&competition_id=244"
+      )
+      .then((resp) => {
+        if (Math.floor(resp.status / 100 === 2)) {
+          console.log(resp);
+          return;
+        }
+      });
+  });
   // {/* <ul>
   //   {usuarios.map((usuario) => (
   //     <li key={usuarios.key}>{usuario.username}</li>
