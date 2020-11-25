b53c07ea3658a13fdb89468fdc1b16f52a6c3501
Marcelo Cesario Miguel <marcelocmiguel@outlook.com>
2020-11-25 09:36:30 -0300

    Create shareButton.test.js

---
+++ src/shareButton.test.js
@@ -0,0 +1,38 @@
+import React, { Component } from'react';
+import PropTypes from "prop-types";
+import Match from "./screens/match";
+import { shallow } from 'enzyme';
+import {FacebookShareButton, TwitterShareButton,WhatsappShareButton,FacebookIcon } from 'react-share';
+
+
+// Código para resolver o conflito do useLocation (mude o location para o url do seu componente)
+jest.mock("react-router-dom", () => ({
+    ...jest.requireActual("react-router-dom"),
+    useLocation: () => ({
+      location: "localhost:3000/match"
+
+    })
+  }));
+
+describe('Share Button', () => {
+    
+    const initialProps = {
+      id: '1',
+      awayTeam: 'Chealse',
+      homeTeam: 'Juventus',
+      score: '3-1',
+      date: '25/11/2020',
+      awayId:'2',
+      homeId:'3',
+      idUser:'4'
+    };
+    const container = shallow(<Match {...initialProps} />);
+  
+    it('Check if Facebook share button was created', () => {
+      expect(container.find(FacebookShareButton))
+    });
+  
+    it('Check if Twitter share button was created', () => {
+        expect(container.find(TwitterShareButton))
+      });
+  });
